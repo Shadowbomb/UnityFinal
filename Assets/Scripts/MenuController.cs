646aa395e4552d80bf8dfa3080a6f2bc59411a86
Christopher-Sparks <csparks1@my.hpu.edu>
2019-04-06 18:06:05 -1000

    MainMenu linked with Game

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public Button play;
+
+    void Start()
+    {
+        play.onClick.AddListener(PlayGame);
+    }
+
+    void PlayGame()
+    {
+
+        SceneManager.LoadScene("Game");
+        Debug.Log("Button clicked");
+    }
+}
